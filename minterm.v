48e98051c613f3f172a12aa6f495d96b368ab87e
Audrey Cruz <audreycruz@u.boisestate.edu>
2026-02-10 10:34:41 -0700

    Implement logic equation for output Y

--- minterm.v
+++ minterm.v
@@ -1,8 +1,19 @@
-module minterm (
-    input A, B, C, D,
-    output Y
-);
-
-assign Y = ; // Enter your equation here
-
-endmodule
+module minterm (                   
+    input A, B, C, D,              
+    output Y                       
+);                                 
+                                   
+                                   
+//assign Y = (B | C | D) &         
+//        (~B  | ~D) &             
+//        (~A |~D);                
+//endmodule                        
+                                   
+                                   
+                                   
+                                   
+assign Y = (C & ~D) |              
+    (~A & ~B  & D) |               
+    (B & ~C & ~D);                 
+endmodule                          
+                                   
